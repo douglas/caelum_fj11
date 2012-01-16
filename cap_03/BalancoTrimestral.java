32030d0d438de6b45d69c731423857ac23f90788
Douglas Soares de Andrade <contato@douglasandrade.com>
2012-01-16 06:11:39 -0200

    Exercicio 3.3.1

---
+++ cap_03/BalancoTrimestral.java
@@ -0,0 +1,11 @@
+class BalancoTrimestral {
+    public static void main(String[] args) {
+        int gastos_janeiro = 15000;
+        int gastos_fevereiro = 23000;
+        int gastos_marco = 17000;
+
+        int gastos_trimestre = gastos_janeiro + gastos_fevereiro + gastos_marco;
+
+        System.out.println("Valor dos gastos no trimestre: " + gastos_trimestre);
+    }
+}
