9154e1583e84a7b3feeb928ad703b11a4c9b82f8
Douglas Soares de Andrade <contato@douglasandrade.com>
2012-01-16 05:54:09 -0200

    Exercicio 2.13.3

--- cap_02/MeuPrograma.java
+++ cap_02/MeuPrograma.java
@@ -1,6 +1,5 @@
 class MeuPrograma {
     public static void main(String[] args) {
-        System.out.println("Minha primeira aplicação Java");
-        System.out.println("Em python eh tao mais facil... mas vamos la");
+        System.out.println("Minha primeira aplicação Java\nEm python eh tao mais facil... mas vamos la");
     }
 }
